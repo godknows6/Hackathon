0249ddfee67f1cac0406c9584dcc0aee8f086687
godknows6 <georgenitin0@gmail.com>
2018-03-23 23:55:52 +0530

    Scene1
    Complete.

--- Hackathon Project/Assets/Vuforia/Scripts/Hackathon/Video_Disp1.cs
+++ Hackathon Project/Assets/Vuforia/Scripts/Hackathon/Video_Disp1.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Video_Disp1 : MonoBehaviour {
-	public GameObject video_plane1;
+	public GameObject video_plane1,video_btn;
 	// Use this for initialization
 	void Start ()
 	{
 		video_plane1 = GameObject.Find ("Video_Plane1");
+		video_btn = GameObject.Find ("Button4");
 		video_plane1.SetActive (false);
+		video_btn.SetActive (false);
 	}
 
 	// Update is called once per frame
@@ -18,12 +20,15 @@ public class Video_Disp1 : MonoBehaviour {
 
 	public void Video_Click()
 	{
-		if (video_plane1.activeSelf) {
+		if (video_plane1.activeSelf)
+		{
 			video_plane1.SetActive (false);
+			video_btn.SetActive (false);
 		}
 		else
 		{
 			video_plane1.SetActive (true);
+			video_btn.SetActive (true);
 		}
 	}
 }
